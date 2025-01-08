e17a3fa5660402b9cadc998b7f9de8ff7f867e99
Cavallari <128869752+cCavallari@users.noreply.github.com>
2025-01-08 00:23:57 -0300

    Update scripts.js
    pluggin de portafolio

--- js/scripts.js
+++ js/scripts.js
@@ -31,4 +31,9 @@ window.addEventListener('DOMContentLoaded', event => {
         });
     });
 
+    // Activate SimpleLightbox plugin for portfolio items
+    new SimpleLightbox({
+        elements: '#portfolio a.portfolio-box'
+    });
+
 });
